0ec1ab706fdb594c7a540f2e292d6c8dfb6a05d2
matjco <matjco12@gmail.com>
2024-01-28 17:13:50 -0500

    Wrote a script in decrypt_message_two.py to decrypt the second message

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,27 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+
+encrypted_message_list = list(encrypted_message)
+message = encrypted_message_list
+
+i = 1
+j = -i - 1
+
+while i < j % len(encrypted_message):
+    j = -i - 1
+
+    #store the earlier character in the message
+    temp = message[i]
+
+    #assign the later character in the message to the earlier character
+    message[i] = message[j]
+
+    #assign the earlier character in the message to the later character
+    message[j] = temp
+
+    i += 2
+
+message_str = "".join(message)
+
+print(message_str)
